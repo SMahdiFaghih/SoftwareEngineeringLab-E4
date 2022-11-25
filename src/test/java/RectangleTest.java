391eaaef721aefbfd7ef63fa09d3dcbeee8b9ca1
mohammadreza <mohammad0reza79@gmail.com>
2022-11-25 13:28:58 +0330

    add another test for rectangle

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -7,6 +7,14 @@ class RectangleTest {
     public void computeAreaTest(){
         Rectangle rectangle = new Rectangle(4,8);
 
+
+        assertEquals(32, rectangle.computeArea());
+    }
+
+    @Test
+    public void computeAreaTest2(){
+        Rectangle rectangle = new Rectangle(4,8);
+
         rectangle.setHeight(rectangle.getHeight() - 1);
         rectangle.setWidth(rectangle.getWidth() + 6);
 
